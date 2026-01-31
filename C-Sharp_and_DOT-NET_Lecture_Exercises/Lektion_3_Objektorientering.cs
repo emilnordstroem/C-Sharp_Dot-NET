08345436561465dcd1f9cbc357c57161e6cbf304
emilnordstroem <emilstoeve@outlook.com>
2026-01-31 19:10:57 +0100

    Completion of task 3.8 - issue of overflow with hour

--- C-Sharp_and_DOT-NET_Lecture_Exercises/Lektion_3_Objektorientering.cs
+++ C-Sharp_and_DOT-NET_Lecture_Exercises/Lektion_3_Objektorientering.cs
@@ -7,7 +7,7 @@ namespace C_Sharp_and_DOT_NET_Lecture_Exercises
 
     internal class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
             List<Shape> shapes = new List<Shape>();
             shapes.Add(new Shape());
@@ -18,9 +18,43 @@ namespace C_Sharp_and_DOT_NET_Lecture_Exercises
             shapes.Add(new Rectangle(2, 2));
             
 			Console.Out.WriteLine(string.Join(", ", shapes));
-			
+            
 
-        }
+            Time t1, t2;
+
+			t1 = new Time("00:00:30");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+			t2.Seconds = t2.Seconds + 29;
+			Console.Out.WriteLine($"Adding 29 Seconds: {t2.ToString()}");
+			t1 = new Time("00:30:00");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+			t2.Minutes = t2.Minutes + 29;
+			Console.Out.WriteLine($"Adding 29 Minutes: {t2.ToString()}");
+			t1 = new Time("12:00:00");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+			t2.Hours = t2.Hours + 11;
+			Console.Out.WriteLine($"Adding 11 Hours: {t2.ToString()}");
+
+			t1 = new Time("23:59:59");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+            t2.Seconds = t2.Seconds + 1;
+			Console.Out.WriteLine($"Adding 1 Second: {t2.ToString()}");
+			t1 = new Time("23:59:00");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+			t2.Minutes = t2.Minutes + 1;
+			Console.Out.WriteLine($"Adding 1 Minute: {t2.ToString()}");
+			t1 = new Time("23:00:00");
+			Console.Out.WriteLine($"Default: {t1.ToString()}");
+			t2 = t1;
+			t2.Hours = t2.Hours + 1;
+			Console.Out.WriteLine($"Adding 1 Hour: {t2.ToString()}");
+
+		}
 	}
 
     internal class Shape
@@ -100,5 +134,69 @@ namespace C_Sharp_and_DOT_NET_Lecture_Exercises
 		}
 
 	}
+
+
+    public struct Time {
+        private int secondsSinceMidnight;
+
+        public Time (string time)
+        {
+			int seconds = Int32.Parse(time.Substring(6, 2));
+			int minutes = Int32.Parse(time.Substring(3, 2));
+			int hours = Int32.Parse(time.Substring(0, 2));
+            secondsSinceMidnight = hours * 3600 + minutes * 60 + seconds;
+        }
+
+        public Time (int hours, int minutes, int seconds)
+        {
+            if (hours > 23 || minutes > 59 || seconds > 59)
+            {
+                throw new InvalidDataException();
+            }
+			secondsSinceMidnight = (hours * 3600) + (minutes * 60) + seconds;
+
+		}
+
+		public int Hours 
+        { 
+            get { return secondsSinceMidnight / 3600; } 
+            set 
+            {
+                int hours = value;
+				int minutes = (secondsSinceMidnight % 3600) / 60;
+				int seconds = secondsSinceMidnight % 60;
+				secondsSinceMidnight = hours * 3600 + minutes * 60 + seconds;
+			}
+		}
+
+        public int Minutes 
+        { 
+            get { return (secondsSinceMidnight % 3600) / 60; } 
+            set
+            {
+                int minutes = value;
+                int hours = secondsSinceMidnight / 3600;
+				int seconds = secondsSinceMidnight % 60;
+				secondsSinceMidnight = hours * 3600 + minutes * 60 + seconds;
+			}
+		}
+
+        public int Seconds 
+        { 
+            get { return secondsSinceMidnight % 60; } 
+            set {
+                int seconds = value;
+                int hours = secondsSinceMidnight / 3600;
+                int minutes = (secondsSinceMidnight % 3600) / 60;
+				secondsSinceMidnight = hours * 3600 + minutes * 60 + seconds;
+			}
+		}
+
+        public override string ToString()
+        {
+            return $"Current Time: {Hours}:{Minutes}:{Seconds}";
+        }
+
+    }
 
 }
