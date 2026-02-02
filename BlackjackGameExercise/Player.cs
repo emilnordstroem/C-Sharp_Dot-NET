f6987d5ecc5b7ff7e2a20d61381de227ec0ef752
emilnordstroem <emilstoeve@outlook.com>
2026-02-02 16:45:14 +0100

    Supports calculating value - placed in player logic. Register players and place bet logic implemented

---
+++ BlackjackGameExercise/Player.cs
@@ -0,0 +1,83 @@
+﻿
+using CardDeck;
+
+namespace Participant
+{
+
+	internal class Player
+    {
+        private List<Card> hand = new List<Card>();
+        public Player (string name, int balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public List<Card> Hand { get { return new List<Card>(hand); } set; }
+        public void AddCardToHand(Card card) { hand.Add(card); }
+        public int CalculateValueOfHand() 
+        {
+            int value = 0;
+            foreach (Card card in hand)
+            {
+                int rank = (int)card.Rank;
+
+				if (rank > 10)
+                {
+                    if (IsAceCardPresent())
+                    {
+                        value += 20;
+                    }
+                    else
+                    {
+						value += 10;
+					}
+				}
+                else if (rank == 1 && IsCourtCardPresent())
+                {
+                    value += 11;
+                }
+                else
+                {
+                    value += rank;
+                }
+            }
+            return value;
+        }
+        public bool IsCourtCardPresent()
+        {
+            foreach (Card card in hand)
+            {
+                if ((int)card.Rank > 10)
+                {
+                    return true;
+                }
+            }
+            return false;
+		}
+        public bool IsAceCardPresent()
+        {
+			foreach (Card card in hand)
+			{
+				if ((int)card.Rank == 1)
+				{
+					return true;
+				}
+			}
+            return false;
+		}
+        public void ClearHand () { hand.Clear(); }
+        public override string ToString()
+        {
+            return $"Player: {Name} [${Balance}]";
+        }
+    }
+
+    internal class Dealer
+	{
+
+    }
+
+}
--- BlackjackGameExercise/Program.cs
+++ BlackjackGameExercise/Program.cs
@@ -1,136 +1,71 @@
 ﻿
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Runtime.InteropServices;
 using CardDeck;
 using Participant;
 
 public class Program
 {
+	private static Deck cardDeck = new Deck();
+
+	private static Dictionary<Player, int> players = new Dictionary<Player, int>();
+
 	static void Main(string[] args)
 	{
-		Console.Out.WriteLine("=========================================");
-		Console.Out.WriteLine($"Blackjack 2026");
-		Console.Out.WriteLine("_________________________________________");
+		Console.Out.WriteLine("====================");
+		Console.Out.WriteLine("Blackjack 2026");
+		Console.Out.WriteLine("____________________");
 
-		Console.Out.WriteLine($"What is your name?");
-		string name = Console.ReadLine();
-		Console.Out.WriteLine($"How much money ($) are you bringing to the table?");
-		int balance = int.Parse(Console.ReadLine());
-		Player player = new Player(name, balance);
-
-		Console.Out.WriteLine("_________________________________________");
-		Console.Out.WriteLine("Let's get started...");
-		Console.Out.WriteLine("_________________________________________");
-
-		House house = new House();
-		Deck deck = new Deck();
-
-		List<Person> participant = [house, player];
+		RegisterPlayers();
 
 		while (true)
 		{
-			try
-			{
-				int placedBet = PlaceBet(ref player);
-			}
-			catch (InvalidDataException error)
-			{
-				Console.Out.WriteLine(error.Message);
-				PlaceBet(ref player);
-			}
+			PlaceBets();
 
-			while (true)
-			{
-				DistributeCards(ref participant, ref deck);
-				CheckValue(ref participant);
-				bool selectOption = SelectOption(player);
-				if (!selectOption)
-				{
-					break;
-				}
-			}
-
-			Console.Out.WriteLine("Do you want to continue playing? - yes/no");
-			string responds = Console.ReadLine();
-			if (responds.ToLower() == "no")
-			{
-				break;
-			}
 		}
-		Console.Out.WriteLine("_________________________________________");
-		Console.Out.WriteLine($"You started ${balance}...");
-		Console.Out.WriteLine($"and you are exiting with ${player.Balance}");
-		Console.Out.WriteLine("=========================================");
+
+
 	}
 
-	static int PlaceBet (ref Player player)
+	static void RegisterPlayers()
 	{
-		Console.Out.WriteLine($"Place a bet");
-		Console.Out.WriteLine($"Limit: ${player.Balance}");
-		int placedBet = int.Parse(Console.ReadLine());
+		Console.Out.WriteLine("How many players will participate?");
+		int numberOfPlayers = int.Parse(Console.ReadLine());
 
-		if (placedBet > player.Balance)
+		while (numberOfPlayers > 0)
 		{
-			throw new InvalidDataException($"Placed bet must be below ${player.Balance}");
-		}
-		player.Balance -= placedBet;
-		return placedBet;
-	}
-
-	static void DistributeCards (ref List<Person> participants, ref Deck deck)
-	{
-		foreach (Person participant in participants)
-		{
-			participant.AddCardToHand(deck.getCard());
-			List<Card> hand = participant.SeeHand();
-			Console.Out.WriteLine("_________________________________________");
-			Console.Out.WriteLine(participant.ToString());
-			Console.Out.WriteLine($"Hand: {string.Join("|", hand.Select(card => card.ToString()))}");
-			Console.Out.WriteLine("_________________________________________");
+			Console.Out.WriteLine("____________________");
+			Console.Out.WriteLine("Enter your name?");
+			string name = Console.ReadLine();
+			Console.Out.WriteLine("How much money do you bring to the table?");
+			int balance = int.Parse(Console.ReadLine());
+			
+			players.Add(new Player(name, balance), 0);
+			numberOfPlayers--;
 		}
 	}
 
-	static void CheckValue (ref List<Person> participants)
+	static void PlaceBets()
 	{
-		foreach (Person participant in participants)
+		foreach (Player player in players.Keys)
 		{
-			int valueOfHand = participant.ValueOfHand();
-			Console.Out.WriteLine("_________________________________________");
-			Console.Out.WriteLine(participant.ToString());
-			Console.Out.WriteLine($"Value: {valueOfHand}");
-			Console.Out.WriteLine("_________________________________________");
-		}
-	}
+			Console.Out.WriteLine("____________________");
+			Console.Out.WriteLine($"{player.Name} enter your bet");
+			Console.Out.WriteLine($"Maximum bet amount: ${player.Balance}");
+			int placedBet = int.Parse(Console.ReadLine());
 
-	static bool SelectOption(Player player)
-	{
-		int valueOfHand = player.ValueOfHand();
-		if (valueOfHand < 21)
-		{
-			Console.Out.WriteLine("_________________________________________");
-			Console.Out.WriteLine(player.ToString());
-			Console.Out.WriteLine("Choose an option: Hit (1), Stand(2)");
-			string responds = Console.ReadLine();
-			if (responds == "1") 
-			{ 
-				return true; 
+			while (placedBet > player.Balance)
+			{
+				Console.Out.WriteLine("____________________");
+				Console.Out.WriteLine($"!! Illegal amount was entered !!");
+				Console.Out.WriteLine($"{player.Name} enter your bet");
+				Console.Out.WriteLine($"Maximum bet amount: ${player.Balance}");
+				placedBet = int.Parse(Console.ReadLine());
 			}
-			else 
-			{ 
-				return false; 
-			}
-		}
-		else if (valueOfHand == 21)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
+
+			Console.Out.WriteLine($"Bet of ${placedBet} has been placed");
+			players[player] = placedBet;
 		}
 	}
+
 
 }
 
