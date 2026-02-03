467aa40e58ed7870bc696d0ca46ff315d57da72b
emilnordstroem <emilstoeve@outlook.com>
2026-02-03 16:32:59 +0100

    Completion of Task 4.2

---
+++ PowerPlantExercise/PowerPlant.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerPlantEntity
+{
+    internal class PowerPlant
+    {
+        public PowerPlant(){ }
+
+        public Warning Warning { get; set; }
+
+        public void HeadUp()
+        {
+            Random randomInteger = new Random();
+            int increaseInTemperature = randomInteger.Next(0, 101);
+            if (increaseInTemperature > 50)
+            {
+                Warning();
+            }
+        }
+
+
+    }
+
+    internal delegate void Warning();
+
+}
---
+++ PowerPlantExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using PowerPlantEntity;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        PowerPlant powerPlant = new PowerPlant();
+        powerPlant.Warning += WarningToConsole;
+        powerPlant.Warning += SucceedToConsole;
+
+        int numberOfInvokes = 10;
+        while (numberOfInvokes > 0)
+        {
+            powerPlant.HeadUp();
+            numberOfInvokes--;
+        }
+
+	}
+
+	public static void SucceedToConsole()
+	{
+		Console.Out.WriteLine("Success!");
+	}
+
+	public static void WarningToConsole()
+    {
+        Console.Out.WriteLine("Warning!");
+    }
+
+}
