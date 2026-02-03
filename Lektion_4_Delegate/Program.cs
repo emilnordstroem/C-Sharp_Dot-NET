fcd0aebba4a129e3b766e5f629c0a94c8b484a08
emilnordstroem <emilstoeve@outlook.com>
2026-02-03 16:05:14 +0100

    Completed task 4.1 and 4.1.a

---
+++ Lektion_4_Delegate/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        String text = "Hello, World!";
+        
+        bool responds = text.Lang();
+        Console.Out.WriteLine(responds);
+
+        responds = MyExtension.Lang(text);
+		Console.Out.WriteLine(responds);
+
+		responds = text.Lang(10);
+		Console.Out.WriteLine(responds);
+
+		responds = MyExtension.Lang(text, 10);
+		Console.Out.WriteLine(responds);
+
+        CalculateAndDisplay(10, 10, Add);
+        CalculateAndDisplay(10, 10, Multiply);
+
+	}
+
+    private static void CalculateAndDisplay(int a, int b, Operation operation)
+    {
+		Console.Out.WriteLine($"A: {a} \nB: {b}");
+        var result = operation(a, b);
+		Console.Out.WriteLine($"Calculated Result: {result}");
+	}
+    private static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    private static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+}
+
+public static class MyExtension
+{
+    public static bool Lang(this String text)
+    {
+        return text.Length > 5;
+    }
+
+	public static bool Lang(this String text, int n)
+	{
+		return text.Length > n;
+	}
+}
+
+public delegate int Operation(int a, int b);
