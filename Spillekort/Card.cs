dc99a7e4476189517ed87ad3ea54ec2e25027883
emilnordstroem <emilstoeve@outlook.com>
2026-02-03 17:48:16 +0100

    Completed task 4.5

---
+++ Spillekort/Card.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Spillekort.Card;
+
+namespace Spillekort
+{
+    public class Card
+    {
+        public Card(Suit suit, Number number)
+        {
+            Suit = suit;
+            Number = number;
+        }
+        public Suit Suit { get; set; }
+        public Number Number { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Suit} : {Number}]";
+        }
+
+		public delegate bool FilterCardDelegate(Card card);
+	}
+
+	public enum Suit
+    {
+        Club = 1,
+        Diamond = 2,
+        Heart = 3,
+        Spade = 4
+    }
+
+	public enum Number
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+
+	public class DeckOfCards
+    {
+        public DeckOfCards()
+        {
+            Deck = new Stack<Card>();
+        }
+        
+        public Stack<Card> Deck { get; set; }
+        public void AddCard(Suit suit, Number number)
+        {
+            Deck.Push(new Card(suit, number));
+        }
+        public Stack<Card> FilterDeck(FilterCardDelegate filter)
+        {
+            return new Stack<Card>(Deck.Where(card => filter(card)));
+        }
+    }
+
+}
---
+++ Spillekort/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Spillekort;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        DeckOfCards deck = new DeckOfCards();
+		deck.AddCard(Suit.Heart, Number.Eight);
+		deck.AddCard(Suit.Club, Number.Jack);
+		deck.AddCard(Suit.Diamond, Number.Queen);
+		deck.AddCard(Suit.Spade, Number.Ace);
+
+		Console.Out.WriteLine(string.Join(",", deck.FilterDeck(FilterByClubs)));
+		Console.Out.WriteLine(string.Join(",", deck.FilterDeck(FilterByCourt)));
+		Console.Out.WriteLine(string.Join(",", deck.FilterDeck(FilterByNonCourt)));
+	}
+
+	static public bool FilterByClubs(Card card)
+    {
+        return card.Suit == Suit.Club;
+    }
+
+	static public bool FilterByCourt(Card card)
+	{
+		return card.Number == Number.Jack
+			|| card.Number == Number.Queen
+			|| card.Number == Number.King;
+	}
+
+	static public bool FilterByNonCourt(Card card)
+	{
+		return card.Number != Number.Jack 
+            && card.Number != Number.Queen 
+            && card.Number != Number.King;
+	}
+
+}
