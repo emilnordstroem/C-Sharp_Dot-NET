6150ef9b5a2a363f5a55c84af4c20d0d342df396
emilnordstroem <emilstoeve@outlook.com>
2026-02-03 16:50:54 +0100

    Completed task 4.3

--- Lektion_4_Delegate/Program.cs
+++ Lektion_4_Delegate/Program.cs
@@ -10,22 +10,33 @@ public class Program
         
         bool responds = text.Lang();
         Console.Out.WriteLine(responds);
-
         responds = MyExtension.Lang(text);
 		Console.Out.WriteLine(responds);
 
 		responds = text.Lang(10);
 		Console.Out.WriteLine(responds);
-
 		responds = MyExtension.Lang(text, 10);
 		Console.Out.WriteLine(responds);
 
+        
         CalculateAndDisplay(10, 10, Add);
         CalculateAndDisplay(10, 10, Multiply);
 
+
+        int factorial = Factorial(4);
+		Console.Out.WriteLine(factorial);
+
+        int factorialInput = 4;
+        factorial = factorialInput.Factorial();
+		Console.Out.WriteLine(factorial);
+        factorial = MyExtensionForInt.Factorial(factorialInput);
+        Console.Out.WriteLine(factorial);
+
+
+
 	}
 
-    private static void CalculateAndDisplay(int a, int b, Operation operation)
+	private static void CalculateAndDisplay(int a, int b, Operation operation)
     {
 		Console.Out.WriteLine($"A: {a} \nB: {b}");
         var result = operation(a, b);
@@ -38,6 +49,15 @@ public class Program
     private static int Multiply(int a, int b)
     {
         return a * b;
+    }
+
+    public static int Factorial(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        return n * Factorial(n - 1);
     }
 
 }
@@ -56,3 +76,15 @@ public static class MyExtension
 }
 
 public delegate int Operation(int a, int b);
+
+public static class MyExtensionForInt
+{
+	public static int Factorial(this int n)
+	{
+		if (n <= 1)
+		{
+			return n;
+		}
+		return n * Factorial(n - 1);
+	}
+}
