21a40a4ac5b05f3e7061a49e9f61bc8a4d78e44a
emilnordstroem <emilstoeve@outlook.com>
2026-02-17 11:43:59 +0100

    Lecture Task 1

---
+++ Lektion_7_HTML_Helpers/Controllers/CountryController.cs
@@ -0,0 +1,39 @@
+﻿using Lektion_7_HTML_Helpers.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lektion_7_HTML_Helpers.Controllers
+{
+	public class CountryController : Controller
+	{
+		// Task 1
+		public IActionResult Index(string SelectedCountry)
+		{
+			List<SelectListItem> countries = new List<SelectListItem>();
+			
+			countries.Add(new SelectListItem { Text = "China", Value = "CN" });
+			countries.Add(new SelectListItem { Text = "Denmark", Value = "DK" });
+			countries.Add(new SelectListItem { Text = "Germany", Value = "DE" });
+			countries.Add(new SelectListItem { Text = "France", Value = "FR" });
+			countries.Add(new SelectListItem { Text = "Sweden", Value = "SE" });
+
+			Utilities.SortSelectedList(countries, SelectedCountry);
+
+			ViewBag.Countries = countries;
+			ViewBag.CountryCode = SelectedCountry;
+
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(IFormCollection formData)
+		{
+			if (ViewBag.Countries == null)
+			{
+				ViewBag.Countries = new List<SelectListItem>();
+			}
+			ViewBag.Countries.Add(new SelectListItem { Text = formData["Country"], Value = formData["Code"] });
+			return View();
+		}
+	}
+}
---
+++ Lektion_7_HTML_Helpers/Program.cs
@@ -0,0 +1,27 @@
+namespace Lektion_7_HTML_Helpers
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapStaticAssets();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Country}/{action=Index}/{id?}")
+				.WithStaticAssets();
+
+			app.Run();
+		}
+	}
+}
