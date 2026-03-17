f67ed49b440944538e8b97f3626f4a00e366f43a
emilnordstroem <emilstoeve@outlook.com>
2026-03-17 18:19:14 +0100

    Partial completion of lecture task - next is 12.5

---
+++ Lektion_12_MAUI/Biler.xaml.cs
@@ -0,0 +1,40 @@
+using System.Collections.ObjectModel;
+using Lektion_12_MAUI.Models;
+using Microsoft.Maui.Controls.Platform;
+
+namespace Lektion_12_MAUI;
+
+public partial class Biler : ContentPage
+{
+	public ObservableCollection<Bil> BilerListe { get; set; } = new();
+
+	// bil bliver behandlet som en property og ikke et data felt
+	public Bil bil { get; set; } = new Bil
+	{
+		Model = "Porsche",
+		Hestekrafter = 300,
+		Eldreven = true
+	};
+
+	public Biler()
+	{
+		InitializeComponent();
+
+		BilerListe.Add(new Bil { Model = "Mercedes", Hestekrafter = 450, Eldreven = true });
+		BilerListe.Add(new Bil { Model = "Opel", Hestekrafter = 120, Eldreven = false });
+
+		BindingContext = this;
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		DisplayAlertAsync("Bil", $"{bil.Model} {bil.Hestekrafter} {bil.Eldreven}", "OK");
+	}
+
+	private void Button_Clicked_1(object sender, EventArgs e)
+	{
+		bil.Model = "Porsche";
+		bil.Hestekrafter = 300;
+		bil.Eldreven = true;
+	}
+}
---
+++ Lektion_12_MAUI/Models/Bil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Lektion_12_MAUI.Models
+{
+	public class Bil : INotifyPropertyChanged
+	{
+		private string _Model;
+		private int _Hestekrafter;
+		private bool _Eldreven;
+
+		public string Model 
+		{ 
+			get
+			{
+				return _Model;
+			}
+			set
+			{
+				_Model = value;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(Model)));
+			}
+		}
+		public int Hestekrafter 
+		{ 
+			get
+			{
+				return _Hestekrafter;
+			}
+			set
+			{
+				_Hestekrafter = value;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(Hestekrafter)));
+			}
+		}
+		public bool Eldreven 
+		{ 
+			get 
+			{
+				return _Eldreven;
+			} 
+			set 
+			{
+				_Eldreven = value;
+				PropertyChanged(this, new PropertyChangedEventArgs(nameof(Eldreven)));
+			}
+		}
+
+		public Bil()
+		{
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+	}
+}
