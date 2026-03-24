1cb7b771a04b2cb7ac69c068ff1bdf771ac4614e
emilnordstroem <emilstoeve@outlook.com>
2026-03-24 14:31:08 +0100

    Demonstration og øvelse i Reqnroll and Gherkins syntaks

---
+++ Lektion_14_Reqnroll/Features/Feature1.feature
@@ -0,0 +1,8 @@
+﻿Feature: Studerende
+
+A short summary of the feature
+
+Scenario: HentStuderende
+	Given Studerende med navn Emil eksistere
+	When Klient klikker på Se studerende
+	Then Studerende med navn Emil fremvises
---
+++ Lektion_14_Reqnroll/StepDefinitions/StuderendeStepDefinitions.cs
@@ -0,0 +1,28 @@
+using System;
+using Reqnroll;
+
+namespace Lektion_14_Reqnroll.StepDefinitions
+{
+    [Binding]
+    public class StuderendeStepDefinitions
+    {
+        [Given("Studerende med navn Emil eksistere")]
+        public void GivenStuderendeMedNavnEmilEksistere()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("Klient klikker på Se studerende")]
+        public void WhenKlientKlikkerPaSeStuderende()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("Studerende med navn Emil fremvises")]
+        public void ThenStuderendeMedNavnEmilFremvises()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
